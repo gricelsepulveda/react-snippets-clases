066d5c68f64c70b58a64d9ffad7775a5332f68c0
Gricel Dayhanna Sepúlveda Rozas <gr3c2l@gmail.com>
2017-10-21 09:38:19 -0300

    adasd

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import './App.css'
 
 class App extends Component {
 
-  state = {
+  state  = {
     value: '',
+    Name: '',
+    LastName: '',
+    Email: '',
     sent: false,
     error: false,
-    data: [
-      
-    ],
+    data: [],
   }
 
   //METODO PARA RECIBIR EL VALOR
 
-  validateValue(value) {
-    const largo = value.length
+  validateValue(Name) {
+    const largo = Name.length
 
     const mensaje = largo === 0
       ? 'Debe ingresar un valor' //SI ES IGUAL A CERO
@@ -27,49 +28,32 @@ class App extends Component {
           : 'Perfect!' //ELSE FINAL
 
     return mensaje
-    // PODRIA SER QUE NO SEA NECESARIO CREAR UNA CONSTANTE Y DIRECTAMENTE RETORNARLO:
-    /*
-    return ( largo == 0
-      ? 'Debe ingresar un valor' //SI ES IGUAL A CERO
-      : largo <= 3 // ANIDANDO UN IF
-        ? 'El valor debe ser mayor que 3'  // SI ES MENOR QUE 3
-        : largo >= 10 //ANIDANDO UN IF
-          ? 'El valor debe ser menor que 10' //SI ES MAYOR QUE 10
-          : 'Perfect!' //ELSE FINAL
-      )
-    */
-    //ALGUNOS PODRÍAN DECIR QUE ES UNA MALA PRÁCTICA PERO SI ES SIMPLE CONVIENE USARLO EN VEZ DE UN SWITCH CASE
+    
   }
 
-  //TODO
-  /*handleInputChange(x) {
-    console.log(x.target.value)
-  }*/
-
-  /*FILTRA DE TODO SÓLO EL TARGET
-  handleInputChange({target}) {
-    console.log(target.value)
-  }*/
 
   //MÁS ESPECIFICO
-  handleInputChange({ target: { value } }) {
-    //console.log(value)
-    this.setState({ value })
+  handleInputChange({ target: { Name } }) {
+    this.setState({ Name })
   }
+
+
 
   //PARA ENVIAR
   enviar(e){
     e.preventDefault()
-    this.validateValue(this.state.value) === 'Perfect!'
+    this.validateValue(this.state.Name) === 'Perfect!'
       ? this.setState({ 
         sent: true, 
         error: false, 
         value: '',
-        data: this.state.data.concat(this.state.value)
+        Name: '',
+        LastName: '',
+        Email: '',
+        data: this.state.data.concat(this.state.Name)
        })
       : this.setState({ error: true})
   }
-
 
   // <input onChange={() => this.handleInputChange()}/> LE INYECTA UN VALOR QUE HACE REFERENCIA AL EVENTO QUE ESÁ PASANDO . X ES EL EVENTO QUE SE ESTÁ GATILLANDO CUANDO ESTOY ACTIVANDO LA FUNCION ONCHANGE 
 
@@ -77,14 +61,65 @@ class App extends Component {
 
     return (
       <div className="App">
-        <form>
-          <input value={this.state.value} onChange={ x => this.handleInputChange(x)}/>
-          <button onClick={(e) => this.enviar(e)}>ENVIAR</button>
+
+        <br/><br/><br/><br/>
+
+        <h1>Registro</h1>
+
+        <form className="full-form">
+          
+          <div className="flex-row start">
+            <label>Nombre:</label>
+            <input type="text" value={this.state.Name} onChange={ x => this.handleInputChange(x)}/>
+          </div>
+
+          <div className="flex-row start">
+            <label>Apellido:</label>
+            <input type="text" value={this.state.LastName} onChange={ x => this.handleInputChange(x)}/>
+          </div>
+
+          <div className="flex-row start">
+            <label>Correo:</label>
+            <input type="text" value={this.state.Email} onChange={ x => this.handleInputChange(x)}/>
+          </div>
+
+          <div className="flex-row start">
+            <label>Sexo:</label>
+            <div className="flex-row start">
+              <input type="radio"/>
+              <label className="inset-label">Femenino</label>
+            </div>
+            <div className="flex-row start">
+              <input type="radio"/>
+              <label className="inset-label">Masculino</label>
+            </div>
+          </div>
+
+          <div className="flex-row start">
+            <label>Música favorita:</label>
+            <div className="flex-row start">
+              <input type="checkbox"/>
+              <label className="inset-label">Rock</label>
+            </div>
+            <div className="flex-row start">
+              <input type="checkbox"/>
+              <label className="inset-label">Pop</label>
+            </div>
+          </div>
+          
+
         </form>
-        <div>{this.validateValue(this.state.value)}</div>
-        {this.state.sent && <div>Formulario enviado con éxito</div>}
-        {this.state.error && <div>El formulario tiene observaciones favor corregir</div>}
+
+        <br/><br/><br/><br/>
+
+
+
+        <div className="alert-state">{this.validateValue(this.state.Name)}</div>
+        {this.state.sent && <div className="alert-state">Formulario enviado con éxito</div>}
+        {this.state.error && <div className="alert-state">El formulario tiene observaciones favor corregir</div>}
         <ul>{this.state.data.map((x, i) => <li key={i}>{x}</li>)}</ul>
+
+
       </div>
     )
   }
